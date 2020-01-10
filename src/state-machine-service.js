96883dae261dad644bfebb0cf1811f631083ce5c
Frito <fritogotlayed@gmail.com>
2020-01-09 21:18:47 -0600

    Implementing state machine client.

--- src/state-machine-service.js
+++ src/state-machine-service.js
@@ -1,5 +1,269 @@
+const got = require('got');
+const urlJoin = require('url-join');
+const { VError } = require('verror');
+
+/**
+ * Initializes a new instance of the Queue Service client
+ *
+ * @param {String} serviceUrl The url base that this client should use for service communication
+ */
 function Client(serviceUrl) {
   this.serviceUrl = serviceUrl;
 }
+
+/**
+ * @typedef {Object} CreateStateMachineResponse
+ * @property {String} uuid The unique identifier of the newly created state machine
+ */
+
+/**
+ * Create a new state machine
+ * @param {String} definition The state machine definition
+ * @returns {Promise<CreateStateMachineResponse|VError>}
+ */
+Client.prototype.createStateMachine = function createStateMachine(definition) {
+  const url = urlJoin(this.serviceUrl, 'machine');
+  const body = typeof definition === 'object' ? JSON.stringify(definition) : definition;
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    throwHttpErrors: false,
+    body,
+  };
+
+  return got.post(url, options)
+    .then((resp) => {
+      switch (resp.statusCode) {
+        case 200: {
+          const parsedBody = JSON.parse(resp.body);
+          return { status: 'created', uuid: parsedBody.uuid };
+        }
+        default:
+          throw new VError({
+            info: {
+              statusCode: resp.statusCode,
+              body: resp.body,
+            },
+          },
+          'An error occurred while creating the state machine.');
+      }
+    });
+};
+
+/**
+ * @typedef {Object} OperationDetail
+ * @property {String} id The unique identifier of the operation
+ * @property {String} created The ISO timestamp when the operation was created
+ * @property {String} status The status of this operation
+ * @property {String} stateKey The user defined key for this operation
+ * @property {Object} input A JSON formatted object for the input
+ * @property {Object} output A JSON formatted object for the output
+ */
+
+/**
+ * @typedef {Object} ExecutionDetailsResponse
+ * @property {String} id The id of the state machine execution
+ * @property {String} status The status of the execution at the time of the request
+ * @property {Array.<OperationDetail>} operations Operations that are associated with this execution
+ */
+
+/**
+ * Get the details of an execution
+ * @param {String} id The id of the state machine execution
+ * @returns {Promise<ExecutionDetailsResponse|null|VError>}
+ */
+Client.prototype.getDetailsForExecution = function getDetailsForExecution(id) {
+  const url = urlJoin(this.serviceUrl, 'execution', id);
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    throwHttpErrors: false,
+  };
+
+  return got.get(url, options)
+    .then((resp) => {
+      switch (resp.statusCode) {
+        case 200:
+          return JSON.parse(resp.body);
+        case 404:
+          return undefined;
+        default:
+          throw new VError({
+            info: {
+              statusCode: resp.statusCode,
+              body: resp.body,
+            },
+          },
+          'An error occurred while obtaining details of the execution.');
+      }
+    });
+};
+
+/**
+ * @typedef {Object} StateMachineDetails
+ * @property {String} id The id of the state machine execution
+ * @property {String} name The friendly name for this state machine
+ * @property {Object} definition The definition for this state machine
+ */
+
+/**
+ * Get the details of a state machine
+ * @param {String} id The id of the state machine
+ * @returns {Promise<StateMachineDetails|null|VError>}
+ */
+Client.prototype.getStateMachine = function getStateMachine(id) {
+  const url = urlJoin(this.serviceUrl, 'machine', id);
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    throwHttpErrors: false,
+  };
+
+  return got.get(url, options)
+    .then((resp) => {
+      switch (resp.statusCode) {
+        case 200:
+          return JSON.parse(resp.body);
+        case 404:
+          return undefined;
+        default:
+          throw new VError({
+            info: {
+              statusCode: resp.statusCode,
+              body: resp.body,
+            },
+          },
+          'An error occurred while obtaining details of the state machine.');
+      }
+    });
+};
+
+/**
+ * @typedef {Object} ExecutionDetails
+ * @property {String} id The id of the state machine execution
+ */
+
+/**
+ * Invoke a new execution of the state machine
+ * @param {String} id The id of the state machine
+ * @param {Object} data The input data for the state machine
+ * @returns {Promise<ExecutionDetails|null|VError>}
+ */
+Client.prototype.invokeStateMachine = function invokeStateMachine(id, data) {
+  const url = urlJoin(this.serviceUrl, 'machine', id, 'invoke');
+  const body = typeof data === 'object' ? JSON.stringify(data) : data;
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    throwHttpErrors: false,
+    body,
+  };
+
+  return got.post(url, options)
+    .then((resp) => {
+      switch (resp.statusCode) {
+        case 200: {
+          const parsedBody = JSON.parse(resp.body);
+          return { status: 'invoked', id: parsedBody.id };
+        }
+        default:
+          throw new VError({
+            info: {
+              statusCode: resp.statusCode,
+              body: resp.body,
+            },
+          },
+          'An error occurred while invoking the state machine.');
+      }
+    });
+};
+
+/**
+ * @typedef {Object} StateMachineListItem
+ * @property {String} id The id of the state machine
+ * @property {String} name The friendly name of the state machine
+ * @property {String} activeVersion The currently active definition for this state machine
+ */
+
+/**
+ * Invoke a new execution of the state machine.
+ * @returns {Promise<Array.<StateMachineListItem>|null|VError>}
+ */
+Client.prototype.listStateMachines = function listStateMachines() {
+  const url = urlJoin(this.serviceUrl, 'machines');
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    throwHttpErrors: false,
+  };
+
+  return got.get(url, options)
+    .then((resp) => {
+      switch (resp.statusCode) {
+        case 200:
+          return JSON.parse(resp.body);
+        default:
+          throw new VError({
+            info: {
+              statusCode: resp.statusCode,
+              body: resp.body,
+            },
+          },
+          'An error occurred while listing the available state machines.');
+      }
+    });
+};
+
+/**
+ * @typedef {Object} UpdateStateMachineResponse
+ * @property {String} uuid The unique identifier of the newly created state machine
+ */
+
+/**
+ * Create a new state machine
+ * @param {String} id The id of the state machine
+ * @param {String} definition The state machine definition
+ * @returns {Promise<UpdateStateMachineResponse|VError>}
+ */
+Client.prototype.updateStateMachine = function updateStateMachine(id, definition) {
+  const url = urlJoin(this.serviceUrl, 'machine', id);
+  const body = typeof definition === 'object' ? JSON.stringify(definition) : definition;
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    throwHttpErrors: false,
+    body,
+  };
+
+  return got.post(url, options)
+    .then((resp) => {
+      switch (resp.statusCode) {
+        case 200: {
+          const parsedBody = JSON.parse(resp.body);
+          return { status: 'updated', uuid: parsedBody.uuid };
+        }
+        default:
+          throw new VError({
+            info: {
+              statusCode: resp.statusCode,
+              body: resp.body,
+            },
+          },
+          'An error occurred while updating the state machine.');
+      }
+    });
+};
 
 module.exports = Client;
---
+++ src/state-machine-service.test.js
@@ -0,0 +1,368 @@
+const chai = require('chai');
+const got = require('got');
+const sinon = require('sinon');
+
+const StateMachineService = require('./state-machine-service');
+
+describe('state-machine-service', () => {
+  beforeEach(() => {
+    this.sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    this.sandbox.restore();
+  });
+
+  describe('createStateMachine', () => {
+    describe('returns status created when new container is created', () => {
+      it('definition is object', () => {
+        // Arrange
+        const postStub = this.sandbox.stub(got, 'post');
+        const definition = { a: 'a', b: 'b' };
+        postStub.returns(Promise.resolve({
+          statusCode: 200,
+          body: JSON.stringify({ uuid: 'abcde12345' }),
+        }));
+        const client = new StateMachineService('http://127.0.0.1:8080');
+
+        // Act
+        return client.createStateMachine(definition)
+          .then((data) => {
+            // Assert
+            const calls = postStub.getCalls();
+            chai.expect(calls.length).to.be.equal(1);
+            chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine');
+            chai.expect(calls[0].args[1].body).to.be.equal(JSON.stringify(definition));
+            chai.expect(data).to.be.eql({ status: 'created', uuid: 'abcde12345' });
+          });
+      });
+
+      it('definition is object', () => {
+        // Arrange
+        const postStub = this.sandbox.stub(got, 'post');
+        const definition = { a: 'a', b: 'b' };
+        postStub.returns(Promise.resolve({
+          statusCode: 200,
+          body: JSON.stringify({ uuid: 'abcde12345' }),
+        }));
+        const client = new StateMachineService('http://127.0.0.1:8080');
+
+        // Act
+        return client.createStateMachine(JSON.stringify(definition))
+          .then((data) => {
+            // Assert
+            const calls = postStub.getCalls();
+            chai.expect(calls.length).to.be.equal(1);
+            chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine');
+            chai.expect(calls[0].args[1].body).to.be.equal(JSON.stringify(definition));
+            chai.expect(data).to.be.eql({ status: 'created', uuid: 'abcde12345' });
+          });
+      });
+    });
+
+    it('errors when status is not 200', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'post');
+      postStub.returns(Promise.resolve({
+        statusCode: 500,
+        body: 'Test error',
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.createStateMachine({ test: 'test' })
+        .then(() => new Error('Test hit then when should hit catch.'))
+        .catch((err) => {
+          chai.expect(err.message).to.be.equal('An error occurred while creating the state machine.');
+        });
+    });
+  });
+
+  describe('getDetailsForExecution', () => {
+    it('returns execution details when execution id is found.', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      const definition = { a: 'a', b: 'b' };
+      postStub.returns(Promise.resolve({
+        statusCode: 200,
+        body: JSON.stringify(definition),
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.getDetailsForExecution('test-123')
+        .then((data) => {
+          // Assert
+          const calls = postStub.getCalls();
+          chai.expect(calls.length).to.be.equal(1);
+          chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/execution/test-123');
+          chai.expect(data).to.be.eql(definition);
+        });
+    });
+
+    it('returns undefinied when execution id is not found', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      postStub.returns(Promise.resolve({
+        statusCode: 404,
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.getDetailsForExecution('test-123')
+        .then((data) => {
+          // Assert
+          const calls = postStub.getCalls();
+          chai.expect(calls.length).to.be.equal(1);
+          chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/execution/test-123');
+          chai.expect(data).to.be.eql(undefined);
+        });
+    });
+
+    it('errors when status is not known', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      postStub.returns(Promise.resolve({
+        statusCode: 500,
+        body: 'Test error',
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.getDetailsForExecution('test-123')
+        .then(() => new Error('Test hit then when should hit catch.'))
+        .catch((err) => {
+          chai.expect(err.message).to.be.equal('An error occurred while obtaining details of the execution.');
+        });
+    });
+  });
+
+  describe('getStateMachine', () => {
+    it('returns active machine definition when machine id is found.', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      const definition = { a: 'a', b: 'b' };
+      postStub.returns(Promise.resolve({
+        statusCode: 200,
+        body: JSON.stringify(definition),
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.getStateMachine('test-123')
+        .then((data) => {
+          // Assert
+          const calls = postStub.getCalls();
+          chai.expect(calls.length).to.be.equal(1);
+          chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine/test-123');
+          chai.expect(data).to.be.eql(definition);
+        });
+    });
+
+    it('returns undefinied when machine id is not found', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      postStub.returns(Promise.resolve({
+        statusCode: 404,
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.getStateMachine('test-123')
+        .then((data) => {
+          // Assert
+          const calls = postStub.getCalls();
+          chai.expect(calls.length).to.be.equal(1);
+          chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine/test-123');
+          chai.expect(data).to.be.eql(undefined);
+        });
+    });
+
+    it('errors when status is not known', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      postStub.returns(Promise.resolve({
+        statusCode: 500,
+        body: 'Test error',
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.getStateMachine('test-123')
+        .then(() => new Error('Test hit then when should hit catch.'))
+        .catch((err) => {
+          chai.expect(err.message).to.be.equal('An error occurred while obtaining details of the state machine.');
+        });
+    });
+  });
+
+  describe('invokeStateMachine', () => {
+    describe('returns status invoked when invocation successful', () => {
+      it('data is object', () => {
+        // Arrange
+        const postStub = this.sandbox.stub(got, 'post');
+        const argData = { a: 'a', b: 'b' };
+        postStub.returns(Promise.resolve({
+          statusCode: 200,
+          body: JSON.stringify({ id: 'abcde12345' }),
+        }));
+        const client = new StateMachineService('http://127.0.0.1:8080');
+
+        // Act
+        return client.invokeStateMachine('test-123', argData)
+          .then((data) => {
+            // Assert
+            const calls = postStub.getCalls();
+            chai.expect(calls.length).to.be.equal(1);
+            chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine/test-123/invoke');
+            chai.expect(calls[0].args[1].body).to.be.equal(JSON.stringify(argData));
+            chai.expect(data).to.be.eql({ status: 'invoked', id: 'abcde12345' });
+          });
+      });
+
+      it('data is object', () => {
+        // Arrange
+        const postStub = this.sandbox.stub(got, 'post');
+        const argData = { a: 'a', b: 'b' };
+        postStub.returns(Promise.resolve({
+          statusCode: 200,
+          body: JSON.stringify({ id: 'abcde12345' }),
+        }));
+        const client = new StateMachineService('http://127.0.0.1:8080');
+
+        // Act
+        return client.invokeStateMachine('test-123', JSON.stringify(argData))
+          .then((data) => {
+            // Assert
+            const calls = postStub.getCalls();
+            chai.expect(calls.length).to.be.equal(1);
+            chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine/test-123/invoke');
+            chai.expect(calls[0].args[1].body).to.be.equal(JSON.stringify(argData));
+            chai.expect(data).to.be.eql({ status: 'invoked', id: 'abcde12345' });
+          });
+      });
+    });
+
+    it('errors when status is not 200', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'post');
+      postStub.returns(Promise.resolve({
+        statusCode: 500,
+        body: 'Test error',
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.invokeStateMachine('test-123', {})
+        .then(() => new Error('Test hit then when should hit catch.'))
+        .catch((err) => {
+          chai.expect(err.message).to.be.equal('An error occurred while invoking the state machine.');
+        });
+    });
+  });
+
+  describe('listStateMachines', () => {
+    it('returns a list of state machines when called.', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      const results = [];
+      postStub.returns(Promise.resolve({
+        statusCode: 200,
+        body: JSON.stringify(results),
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.listStateMachines()
+        .then((data) => {
+          // Assert
+          const calls = postStub.getCalls();
+          chai.expect(calls.length).to.be.equal(1);
+          chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machines');
+          chai.expect(data).to.be.eql(results);
+        });
+    });
+
+    it('errors when status is not known', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'get');
+      postStub.returns(Promise.resolve({
+        statusCode: 500,
+        body: 'Test error',
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.listStateMachines()
+        .then(() => new Error('Test hit then when should hit catch.'))
+        .catch((err) => {
+          chai.expect(err.message).to.be.equal('An error occurred while listing the available state machines.');
+        });
+    });
+  });
+
+  describe('updateStateMachine', () => {
+    describe('returns status updated when machine found', () => {
+      it('definition is object', () => {
+        // Arrange
+        const postStub = this.sandbox.stub(got, 'post');
+        const definition = { a: 'a', b: 'b' };
+        postStub.returns(Promise.resolve({
+          statusCode: 200,
+          body: JSON.stringify({ uuid: 'abcde12345' }),
+        }));
+        const client = new StateMachineService('http://127.0.0.1:8080');
+
+        // Act
+        return client.updateStateMachine('abcde12345', definition)
+          .then((data) => {
+            // Assert
+            const calls = postStub.getCalls();
+            chai.expect(calls.length).to.be.equal(1);
+            chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine/abcde12345');
+            chai.expect(calls[0].args[1].body).to.be.equal(JSON.stringify(definition));
+            chai.expect(data).to.be.eql({ status: 'updated', uuid: 'abcde12345' });
+          });
+      });
+
+      it('definition is object', () => {
+        // Arrange
+        const postStub = this.sandbox.stub(got, 'post');
+        const definition = { a: 'a', b: 'b' };
+        postStub.returns(Promise.resolve({
+          statusCode: 200,
+          body: JSON.stringify({ uuid: 'abcde12345' }),
+        }));
+        const client = new StateMachineService('http://127.0.0.1:8080');
+
+        // Act
+        return client.updateStateMachine('abcde12345', JSON.stringify(definition))
+          .then((data) => {
+            // Assert
+            const calls = postStub.getCalls();
+            chai.expect(calls.length).to.be.equal(1);
+            chai.expect(calls[0].args[0]).to.be.equal('http://127.0.0.1:8080/machine/abcde12345');
+            chai.expect(calls[0].args[1].body).to.be.equal(JSON.stringify(definition));
+            chai.expect(data).to.be.eql({ status: 'updated', uuid: 'abcde12345' });
+          });
+      });
+    });
+
+    it('errors when status is not 200', () => {
+      // Arrange
+      const postStub = this.sandbox.stub(got, 'post');
+      postStub.returns(Promise.resolve({
+        statusCode: 500,
+        body: 'Test error',
+      }));
+      const client = new StateMachineService('http://127.0.0.1:8080');
+
+      // Act
+      return client.updateStateMachine('abcde12345', {})
+        .then(() => new Error('Test hit then when should hit catch.'))
+        .catch((err) => {
+          chai.expect(err.message).to.be.equal('An error occurred while updating the state machine.');
+        });
+    });
+  });
+});
